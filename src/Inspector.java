c8ee50688e285286a1ab5c697a1ceb59d44f4c48
shshunda <shshunda@ZONE04-WD.cs.ucalgary.ca>
2016-10-18 11:10:03 -0600

    Initialize project: Main and Inspector classes added

---
+++ src/Inspector.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author shshunda
+ */
+public abstract class Inspector {
+    
+    public abstract void inspect(Object obj, boolean recursive);
+    
+}
