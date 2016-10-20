8c0b27a3dd34495941b4ed72dea496bc77ac4781
shshunda <shshunda@ZONE04-WD.cs.ucalgary.ca>
2016-10-19 18:45:55 -0600

    Create mock Class and Interface for testing purposes.

---
+++ src/ClassA.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author shshunda
+ */
+public class ClassA implements InterfaceA {
+
+    @Override
+    public void a() {
+        System.out.println("This is a mock object from a().");
+    }
+    
+}
